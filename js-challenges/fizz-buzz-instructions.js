a934ffc95d10c438bccd41d0df2e02ae77d61acd
Marco <marco@Marcos-Mac-mini.local>
2017-08-07 13:43:56 +0100

    fizzbuzz updated

--- js-challenges/fizz-buzz-instructions.js
+++ js-challenges/fizz-buzz-instructions.js
@@ -21,3 +21,18 @@ if the number is divisible by both we print out "FizzBuzz" */
 "FizzBuzz"
 etc...
 */
+
+
+
+for (var i = 1; i <= 100; i++) {
+
+    if (!(i % 3) && !(i % 5)) {
+        console.log("Fizz");
+   }  else if (!(i % 5)) {
+        console.log("Buzz");          
+    } else if (!(i % 3)) {
+        console.log("FizzBuzz");
+    } else {
+        console.log(i);
+    }
+}
