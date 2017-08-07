ec3cd9316bf376238af812224d4623d0a296ff3e
Rodrigo Alves <rodrigo@swogo.com>
2017-08-07 11:21:12 +0100

    Added fizz-buzz exercise

---
+++ js-challenges/fizz-buzz-instructions.js
@@ -0,0 +1,23 @@
+/* We wanna make a programs that countes from 1 to 100 and outputs the numbers,
+but every time one number is divisible by 3 we print out "Fizz"
+and every time one number is divisible by 5 we print out "Buzz"
+if the number is divisible by both we print out "FizzBuzz" */
+
+/* Expected output
+1
+2
+"Fizz"
+4
+"Buzz"
+"Fizz"
+7
+8
+"Fizz"
+"Buzz"
+11
+"Fizz"
+13
+14
+"FizzBuzz"
+etc...
+*/
